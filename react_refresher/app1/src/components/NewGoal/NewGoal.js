c420ec3464973192ac34cc3fadf65f4cffa62b76
Ishan Sharma <ishan@mtlbigger.com>
2021-07-05 10:18:27 +0530

    addGoal working

--- react_refresher/app1/src/components/NewGoal/NewGoal.js
+++ react_refresher/app1/src/components/NewGoal/NewGoal.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewGoal.css';
 
+
 const NewGoal = props =>{
+    const [entText,setText] = useState('');
+    
     const addGoalHandler  = event =>{
         event.preventDefault();
-        props.new("My New Goal");
+        props.new(entText);
+        setText('');
+        
     };
     
+    const txtHandler = event =>{
+        setText(event.target.value);
+    };
+  
+    
+//   console.log(entText);  
     
     
-    
+      
     return (
         <form className="new-goal" onSubmit = {addGoalHandler}>
-            <input type="text"/>
+            <input type="text" value={entText} onChange={txtHandler}/>
             <button type="submit">Add Goal</button>
         </form>
     );
